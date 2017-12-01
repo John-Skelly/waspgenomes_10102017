cd8005afa5d2e05eedf2d3549208a72a585a81e2
John-Skelly <john.skelly@outlook.co.nz>
2017-12-01 13:51:09 +1300

    did some R things

--- src/Stats_Rscript.R
+++ src/Stats_Rscript.R
@@ -1,3 +1,7 @@
+install.packages("data.table")
+install.packages("ggplot2")
+install.packages("bit64")
+
 library(data.table)
 library(ggplot2)
 library(bit64)
@@ -8,8 +12,26 @@ stats[ ,assembly_ID:= gsub(".*/meraculous/(.+)/meraculous_gap_closure.*", "\\1",
 
 stats[ , c("strain", "read_set", "k", "diploid_mode") := tstrsplit(assembly_ID, "/")]
 
-plot_data <- melt(stats,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c("n_scaffolds", "scaf_bp", "scaf_N50"))
+plot_data_all <- melt(stats,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c("n_scaffolds", "scaf_bp", "scaf_N50"))
+
+plot_data_n <- melt(stats,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c("n_scaffolds"))
+
+plot_data_bp <- melt(stats,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c("scaf_bp"))
+
+plot_data_N50 <- melt(stats,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c( "scaf_N50"))
 
 ggplot(plot_data, aes(x = diploid_mode, y = value, fill = read_set))+
+  facet_grid(strain+variable ~ k, scales = "free_y")+
+  geom_col(position = "dodge")
+
+ggplot(plot_data_n, aes(x = diploid_mode, y = value, fill = read_set))+
+  facet_grid(strain+variable ~ k, scales = "free_y")+
+  geom_col(position = "dodge")
+
+ggplot(plot_data_bp, aes(x = diploid_mode, y = value, fill = read_set))+
+  facet_grid(strain+variable ~ k, scales = "free_y")+
+  geom_col(position = "dodge")
+
+ggplot(plot_data_N50, aes(x = diploid_mode, y = value, fill = read_set))+
   facet_grid(strain+variable ~ k, scales = "free_y")+
   geom_col(position = "dodge")

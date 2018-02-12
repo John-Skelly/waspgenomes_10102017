f75490f79bfba4cc3e072c466b9a0ef675243826
John-Skelly <john.skelly@outlook.co.nz>
2018-02-12 13:48:37 +1300

    removed manual dmin inputs for MA3

--- Snakefile
+++ Snakefile
@@ -201,19 +201,7 @@ rule meraculous:
         'output/meraculous/{strain}/{read_set}/meraculous.log'
     run:
         my_fastq = resolve_path(input.fastq)
-        if wildcards.strain == 'MA3':
-            if wildcards.k == '31':
-                my_dmin = '46'
-            elif wildcards.k == '71':
-                my_dmin = '38'
-            elif wildcards.k == '127':
-                my_dmin = '26'
-            else:
-                raise ValueError('kmer not matched or whatever it does not really matter')
-            if wildcards.read_set == 'norm':
-                my_dmin = str(int(my_dmin)//2)
-        else:
-            my_dmin = '0'
+        my_dmin = '0'
         my_conf = meraculous_config_string.format(
             my_fastq, wildcards.k, wildcards.diploid_mode, my_dmin, threads)
         with open(output.config, 'wt') as f:
--- src/Stats_Rscript.R
+++ src/Stats_Rscript.R
@@ -12,7 +12,7 @@ setkey(stats, strain, read_set, k, diploid_mode)
 
 stats_with_NA <- stats[CJ(unique(strain), unique(read_set), unique(k), unique(diploid_mode))]
 
-stats_with_NA[,k:=factor(k,levels=c("k_31", "k_71", "k_127")) ]
+stats_with_NA[,k:=factor(k,levels=c("k_31", "k_63", "k_67", "k_71", "k_75", "k_79", "k_127")) ]
 
 plot_data_all <- melt(stats_with_NA,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c("n_scaffolds", "scaf_bp", "scaf_N50"))
 
@@ -24,13 +24,17 @@ plot_data_N50 <- melt(stats_with_NA,id.vars = c("strain", "read_set", "k", "dipl
 
 plot_data_L50 <- melt(stats_with_NA,id.vars = c("strain", "read_set", "k", "diploid_mode") , measure.vars = c( "scaf_L50"))
 
-ggplot(plot_data_all, aes(x = diploid_mode, y = value, fill = read_set))+
+ggplot(plot_data_all, aes(x = diploid_mode, y = value, fill = read_set, label=value))+
   facet_grid(strain+variable ~ k, scales = "free_y")+
-  geom_col(position = "dodge")
+  geom_col(position = "dodge")+
+  geom_text(size = 3, check_overlap = TRUE, position = position_stack(0.5)
+            )
+  
 
 ggplot(plot_data_n, aes(x = diploid_mode, y = value, fill = read_set))+
   facet_grid(strain+variable ~ k, scales = "free_y")+
   geom_col(position = "dodge")
+  
 
 ggplot(plot_data_bp, aes(x = diploid_mode, y = value, fill = read_set))+
   facet_grid(strain+variable ~ k, scales = "free_y")+
@@ -43,3 +47,4 @@ ggplot(plot_data_N50, aes(x = diploid_mode, y = value, fill = read_set))+
 ggplot(plot_data_L50, aes(x = diploid_mode, y = value, fill = read_set))+
   facet_grid(strain+variable ~ k, scales = "free_y")+
   geom_col(position = "dodge")
+

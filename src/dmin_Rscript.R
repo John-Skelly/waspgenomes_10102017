dcfbca72621c04bfbe04bdd538353e1bf4cc86b5
John-Skelly <john.skelly@outlook.co.nz>
2018-03-23 11:33:56 +1300

    added dmin_finder, plot_kmer_coverage and added conditional dmin to meraculous

--- src/dmin_Rscript.R
+++ src/dmin_Rscript.R
@@ -1,22 +1,37 @@
+install.packages("dplyr")
+install.packages("tidyr")
 library(data.table)
 library(ggplot2)
+library(tidyr)
+library("stringi")
 
 mercount_files <- list.files('output', 'mercount.hist', recursive = TRUE, full.names = TRUE)
 
-names(mercount_files) <- gsub(pattern = '^.+MA3/(.+)/meraculous_mercount.+$', replacement = '\\1' , mercount_files)
+names(mercount_files) <- stri_replace_all_regex(mercount_files, '(.+)(/)(.+)(/)(.+)(/)(.+)(/)(.+)(/)(.+)(/)meraculous_mercount.+$', replacement = '$5 $7 $9 $11'  )
 
 mercount_list <- lapply(mercount_files, fread, col.names = c('depth', 'kmers'))
 
-mercount_plot <- rbindlist(mercount_list, idcol = 'readset')
+mercount_plot <- rbindlist(mercount_list, idcol = 'param')
 
-ggplot(mercount_plot, aes(x = depth, y = kmers, colour = readset))+
+mercount_plot_param <- split(mercount_plot, mercount_plot$param)
+
+for (assemblies in mercount_plot_param)
+
+MA3_trim_decon_k_127_diplo1 <- data.frame(mercount_plot_param["MA3 trim_decon k_127 diplo_1"])
+
+names(MA3_trim_decon_k_127_diplo1) <- c("param", "depth", "kmers")
+
+MA3_trim_decon_k_127_diplo1
+
+ggplot(MA3_trim_decon_k_127_diplo1, aes(x = depth, y = kmers, colour = params))+
   geom_path()+
   xlim(c(0, 200))+
-  scale_y_log10()+
-  facet_wrap(~readset, scales = 'free_y')+
-  geom_vline(xintercept = c(19, 38))
+  scale_y_log10()#+
+  #facet_wrap(~params, scales = 'free_y')+
+  #geom_vline(xintercept = c(19, 38))
 
-mercount_plot[readset == 'trim_decon', diff(diff(kmers) >0) !=0]
+
+mercount_plot_param[param == 'trim_decon', diff(diff(kmers) >0) !=0]
 
 mercount_plot[readset == 'norm', diff(diff(kmers) >0) !=0]
 
---
+++ src/dmin_finder.R
@@ -0,0 +1,46 @@
+#loads libraries
+library(data.table)
+library(ggplot2)
+library("stringi")
+
+#input and output
+mercount_file <- snakemake@input[["mercount_file"]]
+dmin_plot <- snakemake@output[["dmin_plot"]]
+dmin_out <- snakemake@output[["dmin_out"]]
+log_file <- snakemake@output[["log"]]
+
+#set log
+log <- file(log_file, open = "wt")
+sink(log, type = "message")
+sink(log, append = TRUE, type = "output")
+
+#adds identifier parameters to name 
+names(mercount_file) <- stri_replace_all_regex(mercount_file, 
+                                                '(.+)(/)(.+)(/)(.+)(/)(.+)(/)(.+)(/)(.+)(/)meraculous_mercount.+$', 
+                                                replacement = '$5 $7 $9 $11'  )
+
+#reads in file
+mercount_data <- fread(mercount_file, col.names = c('depth', 'kmers'))
+
+#adds name coloumn 
+mercount_plot <- rbind2(mercount_data, idcol = 'param')
+
+#returns dmin, constructs graph and indicates dmin
+TRUE_kmers<- (which(((diff(diff(mercount_plot$kmers) >0) !=0))))
+dmin <- TRUE_kmers[1]
+dmin_plot <- ggplot(mercount_plot, aes(x = depth, y = kmers, colour = "red"))+
+        geom_path()+
+        xlim(c(0, 200))+
+        scale_y_log10()+
+        geom_vline(xintercept= dmin, linetype= "dashed", color= "blue")
+
+#write output
+ggsave(filename = dmin_plot,
+       plot = dmin_plot,
+       device = cairo_pdf,
+       width =10,
+       height = 7.5,
+       units = "in")
+
+# write session info
+sessionInfo()

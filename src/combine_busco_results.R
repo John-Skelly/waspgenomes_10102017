f4102f93cea82fd6639fdc96f5f516284a8f379f
John-Skelly <john.skelly@outlook.co.nz>
2018-08-27 10:00:35 +1200

    added png output

--- src/combine_busco_results.R
+++ src/combine_busco_results.R
@@ -48,6 +48,6 @@ gp <- ggplot(busco_stats,aes(x=k, y=percent_complete, fill=Status))+
 
 saveRDS(busco_stats, file = output_rds)
 
-ggsave("out_plot.png", plot = gp)
+ggsave("out_plot.png", plot = gp, device = "png")
 ggsave(filename = output_plot, plot = gp, width = 10, height = 7.5, units = 'in')
 
